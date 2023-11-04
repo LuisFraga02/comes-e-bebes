b2da9e6e4b8d7e080e96eb09a222a476dad78ab5
LuisFraga <luiseduardofragawermann@gmail.com>
2023-11-04 07:13:37 -0300

    Update frontend files for login and main page

--- frontend/src/components/main/mainPage.tsx
+++ frontend/src/components/main/mainPage.tsx
@@ -1,8 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 function mainPage() {
-  return (
-    <div>
-      <h1>mainPage</h1>
-    </div>
-  );
+	const navigate = useNavigate();
+
+	const checkAuth = () => {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			navigate('/');
+		}
+	};
+
+	useEffect(() => {
+		return () => clearInterval(setInterval(checkAuth, 500));
+	}, []);
+
+	return (
+		<div>
+			<h1>mainPage</h1>
+			<h2>{localStorage.getItem('token')}</h2>
+			<h3>{localStorage.getItem('username')}</h3>
+		</div>
+	);
 }
 export default mainPage;
